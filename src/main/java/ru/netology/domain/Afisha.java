8aa0e8202491653a70b547b28a99bd0cb7a8da61
larlarlar <larisa.gurina.s@gmail.com>
2021-02-26 22:57:07 +0300

    updated code

---
+++ src/main/java/ru/netology/domain/Afisha.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+
+public class Afisha {
+    private int id;
+    private String movieName;
+    private String moviePoster;
+    private String movieGenre;
+
+}
