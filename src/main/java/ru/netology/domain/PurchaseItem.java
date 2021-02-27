6f8af3aa10a8ebcf3b9abd4da59c8f07105a0b6c
larlarlar <larisa.gurina.s@gmail.com>
2021-02-27 13:11:08 +0300

    old code no tests

--- src/main/java/ru/netology/domain/PurchaseItem.java
+++ src/main/java/ru/netology/domain/PurchaseItem.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor;
-@Data;
-@NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class PurchaseItem {
     private int id;
